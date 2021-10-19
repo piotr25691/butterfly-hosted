f9ee8c502b7037099b250952a34eeb886926b263
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2021-10-19 19:09:08 +0200

    fixes

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import subprocess
 import os
-import socket
 
 curr_env = {}
 curr_env.update(os.environ)
 
 if __name__ == "__main__":
-  os.system("chmod 777 ./java/bin/java")
-  butterfly = subprocess.Popen("./java/bin/java -jar butterfly.jar", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, env=curr_env)
-  while True:
-    for line in iter(butterfly.stdout.readline, b''):
-      line = line.rstrip().decode("iso-8859-1")
-      if line:
-        print(line)
-      elif not line:
-          break       
+    os.system("chmod 777 ./java/bin/java")
+    butterfly = subprocess.Popen("./java/bin/java -jar butterfly.jar", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+                                 env=curr_env, shell=True)
+    while True:
+        for line in iter(butterfly.stdout.readline, b''):
+            line = line.rstrip().decode("iso-8859-1")
+            if line:
+                print(line)
+            else:
+                break
