937367965008024bb6ebb573872408608882dd83
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2021-10-20 17:18:31 +0200

    python script for linux/windows

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ curr_env = {}
 curr_env.update(os.environ)
 
 if __name__ == "__main__":
-    os.system("chmod 777 ./java/bin/java")
-    butterfly = subprocess.Popen("./java/bin/java -jar butterfly.jar", stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
-                                 env=curr_env, shell=True)
-    while True:
-        for line in iter(butterfly.stdout.readline, b''):
-            line = line.rstrip().decode("iso-8859-1")
-            if line:
-                print(line)
-            else:
-                break
+    if os.name != "nt":
+        os.system("chmod 777 ./java/bin/java") 
+        butterfly = subprocess.Popen("./java-min/linux/bin/java -jar butterfly.jar", stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT, env=curr_env, shell=True)
+    else:
+        butterfly = subprocess.Popen(r"%cd%\java-min\win\bin\java -jar butterfly.jar", stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT, env=curr_env, shell=True)
+    while butterfly.stdout.readable():
+        line = butterfly.stdout.readline().decode("iso-8859-1")
+        if not line:
+            break
+        print(line)
