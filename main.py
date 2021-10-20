d5e63f761722e677a729465dd7f1cd28aa4cca71
piotr25691 <71488561+piotr25691@users.noreply.github.com>
2021-10-20 17:42:07 +0200

    re-up without lfs

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:6e27e4016b483a8415affdbd145902dd30604aafa6e880cd3e51e201f15f81ad
-size 741
+import subprocess
+import os
+
+curr_env = {}
+curr_env.update(os.environ)
+
+if __name__ == "__main__":
+    if os.name != "nt":
+        os.system("chmod 777 ./java/bin/java") 
+        butterfly = subprocess.Popen("./java-min/linux/bin/java -jar butterfly.jar", stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT, env=curr_env, shell=True)
+    else:
+        butterfly = subprocess.Popen(r"%cd%\java-min\win\bin\java -jar butterfly.jar", stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT, env=curr_env, shell=True)
+    while butterfly.stdout.readable():
+        line = butterfly.stdout.readline().decode("iso-8859-1")
+        if not line:
+            break
+        print(line)
